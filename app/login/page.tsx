03de54ed48d6f444c8864075cb4ad0b06b5d2d54
Sadman Sakib <106551185+Sadman-Sakib-AIUB@users.noreply.github.com>
2025-10-14 01:19:05 +0600

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -43,7 +43,7 @@ export default function Login() {
         { withCredentials: true }
       );
 
-      router.push(`${process.env.NEXT_PUBLIC_URL}/dashboard`);
+      router.push("/dashboard");
     } catch (error: any) {
       const message = error.response?.data?.message || "Invalid Email and Password";
 
